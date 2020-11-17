2bdce56917da71974150593eebec38ae938d3709
Vincent-Xavier JUMEL <endymion@thetys-retz.net>
2020-11-17 11:24:06 +0100

    Ajout d'une fonction pour afficher les erreurs

--- projet.py
+++ projet.py
@@ -3,6 +3,9 @@ import logging
 def affiche(message):
     return logging.warning(message)
 
+def erreur(message):
+    return loggin.error(message)
+
 logging.debug('Message')
 logging.warning('Attention')
 
