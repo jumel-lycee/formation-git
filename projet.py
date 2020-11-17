e332c2cfc627d18225d816f8c2b1e3b15c0b1a1a
Vincent-Xavier JUMEL <endymion@thetys-retz.net>
2020-11-17 13:23:14 +0100

    suppression d'une fonction

--- projet.py
+++ projet.py
@@ -3,8 +3,8 @@ import logging
 def affiche(message):
     return logging.warning(message)
 
-def erreur(message):
-    return logging.error(message)
+
+
 
 logging.debug('Message')
 logging.warning('Attention')
