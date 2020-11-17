ddc85ac1b394f3c4c61aa8f6f670335dc4c3fc04
Vincent-Xavier JUMEL <endymion@thetys-retz.net>
2020-11-17 13:27:44 +0100

    Modification de la fonction

--- projet.py
+++ projet.py
@@ -3,7 +3,9 @@ import logging
 def affiche(message):
     return logging.warning(message)
 
-
+def erreur(message)
+    logging.info(message)
+    return logging.error(message)
 
 
 logging.debug('Message')
